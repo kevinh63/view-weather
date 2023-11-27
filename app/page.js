28c6fb00e74badc76bbe32ff6abd15a9955485c3
kevinh63 <kevin.h.huang@edu.sait.ca>
2023-11-26 19:05:47 -0700

    added grids

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
-export default function page(){
-  return(
-    <main>
-      <h1>Test2</h1>
+export default function Home() {
+  return (
+    <main className="bg-gray-600 min-h-screen">
+      <h1 className="text-center">View Weather in AB and BC</h1>
+
+      {/* this is the grid */}
+      <div className="bg-gray-600min-h-screen grid row-span-auto col-span-auto">
+        {/* section 1 */}
+        <div className="grid col-start-1 col-end-2 text-center">Alberta</div>
+        {/* section 2 */}
+        <div className="grid col-start-2 col-end-3 text-center">British Columbia</div>
+        {/* section 3 */}
+        <div className="grid col-start-1 col-end-2 row-start-2 text-center">section 3</div>
+        {/* section 4 */}
+        <div className="grid col-start-2 col-end-3 row-start-2 text-center">section 4</div>
+      </div>
     </main>
   )
 }
